dba1a0d68f37518c8f56a89034beb1f05ef84095
Vang, Ka Zoua <200164035@fvtc.edu>
2026-09-12 19:46:22 -0500

    done with demo

---
+++ site.js
@@ -0,0 +1,15 @@
+
+// Exercise 1
+
+//const mainContentSection = document.querySelector('h2')
+//mainContentSection.style.color = 'red'
+
+// Exercise 2
+
+//const link = document.querySelector('footer a')
+
+//onst link = document.querySelector('#fork')
+//link.style.textDecoration = 'underline'
+
+document.body.style.backgroundColor = 'lightgray'
+document.title = "Ryan's Webpage"
